55597d3a608a4f286d75bd2bf2dffb657a47298d
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-28 18:44:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,17 @@
 # Write your code here!
+def game_h
+  {
+    hasket : {
+      team_name: "Brooklyun Nets",
+      colors:["black", "White"],
+      players: [
+        {:player_name => "Alan Anderson",
+        :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1},
+          ]
+  }
