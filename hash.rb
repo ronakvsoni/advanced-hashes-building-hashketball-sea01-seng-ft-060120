a65d041986acf9c311a48b3ae689f3b35b540368
ronakvsoni <ronakvsoni@gmail.com>
2020-06-29 09:14:02 -0700

    Done.

---
+++ hash.rb
@@ -0,0 +1,27 @@
+team_name: "Brooklyn Nets"
+colors: ["Black", "White"]
+players:[
+  {
+  player_name: ""
+  number:
+  shoe:
+  points:
+  rebounds:
+  assists:
+  steals:
+  blocks:
+  slam_dunks:
+}
+]
+
+
+
+player_name: ""
+number:
+shoe:
+points:
+rebounds:
+assists:
+steals:
+blocks:
+slam_dunks:
